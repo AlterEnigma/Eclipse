974d968250fc4a5856f88063b84bdb5fc9d7a43f
Reisone <reisonantia@gmail.com>
2023-09-21 23:35:36 -0600

    Create [Template] Twitter Artist Feed (chaesuart).js
    Initial version of this code template.

---
+++ Cloud Platforms/IFTTT/Twitter Templates/[Template] Twitter Artist Feed (chaesuart).js
@@ -0,0 +1,32 @@
+// TypeScript v2.92
+let imageURL = Feed.newFeedItem.EntryImageUrl;
+let noImage = 'no_image_card.png';
+let regex_pattern = /@/i;
+var iconUrl_init = String.prototype.concat('https://avatar-resolver.vercel.app/twitter/', Feed.newFeedItem.EntryAuthor);
+var iconUrl_final = iconUrl_init.replace(regex_pattern, '');
+var name_init = String.prototype.concat('chaesu👟 (', Feed.newFeedItem.EntryAuthor, ')');
+var text_init = String.prototype.concat('Twitter • ', Feed.newFeedItem.EntryPublished);
+var body = {
+  embeds: [
+    {
+      color: 1942002,
+      author: {
+        name: name_init,
+        url: Feed.newFeedItem.EntryUrl,
+        icon_url: iconUrl_final,
+      },
+      description: Feed.newFeedItem.EntryTitle,
+      image: {
+        url: Feed.newFeedItem.EntryImageUrl,
+      },
+      footer: {
+        text: text_init,
+        icon_url: 'https://i.imgur.com/s2qjM73.png',
+      },
+    },
+  ],
+};
+if (imageURL.indexOf(noImage) !== -1) {
+  MakerWebhooks.makeWebRequest.skip();
+}
+MakerWebhooks.makeWebRequest.setBody(JSON.stringify(body));
